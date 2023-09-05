53010c1833dce26546ff7995e6a852bb34b0b3e8
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-09-05 15:00:00 -0500

    Add files via upload
    new files

---
+++ Scope-hoisting-closure test/main.js
@@ -0,0 +1,88 @@
+
+"use strict"
+
+// let game="sonic"
+
+// console.log(game)
+
+
+// function title(){
+//     let game="knuckles"
+//     console.log(game)
+//     let game2="tails"
+// }
+
+// title()
+
+// // if(true){
+// //     let game="mario"
+// //     console.log(game)
+// // }
+
+
+// if(true){
+//     let game="mario"
+//     console.log(game)
+// }
+
+
+// // if(true){
+// //     console.log(game2) //dont work locally scoped
+// // }
+
+
+// function hello(){
+//     var greeting="hello"
+// }
+
+
+// console.log(greeting) //doesnt work
+
+// if (true){
+//     var plumber="mario" //will work var only limted by function scoped
+// }
+
+// console.log(plumber)
+
+
+// 1)
+// let firstName="mike"
+// const lastName="henley"
+// let age=23
+
+// function nameLog(){
+//     console.log(firstName + " " + lastName + " is " + age + " years old")
+// }
+
+// nameLog()
+
+
+
+//2.)
+// function nameLog(){
+//     let country="england"
+//     let firstName="mike"
+//     const lastName="henley"
+//     let age=23
+//     console.log(firstName + " " + lastName + " is " + age + " years old and lives in " + country)
+// }
+
+// nameLog()
+
+// //3.)
+// firstName="john"
+// //yes problem...not defined ...other name is locally scoped. 
+
+//4.)
+// let game="sonic"
+
+// if(true){
+//     let game="mario"
+//     console.log(game)
+// }
+
+// console.log(game)
+
+//anwser is 
+//mario
+//sonic
