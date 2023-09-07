7543a9492fe70d748cc9d670f8f0f27d89d9f9b2
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-09-06 20:46:45 -0500

    Add files via upload
    Tested running javascript code and using variables from module and nonModule files

---
+++ Module Scope and nonModule/main.js
@@ -0,0 +1,7 @@
+console.log(globalVar);
+
+import { exportedVar } from "./module.js";
+
+console.log(exportedVar);
+
+// console.log(moduleVar); wont work because its a module scoped variable not exported
