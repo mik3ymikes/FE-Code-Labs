bcd15cc1244e64e30e1289c0055c358a7cb11f3a
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-09-17 22:40:50 -0500

    started dom lessons

---
+++ Dom manipuloaiton/main.js
@@ -0,0 +1,28 @@
+// const body=document.querySelector('body')
+
+// body.style.backgroundColor="red"
+
+// // function gococo(){
+// //   body.style.backgroundColor="green"
+// // }
+
+// let btn=document.querySelectorAll(".btn")
+
+// for(let i=0;i<btn.length;i++){
+// btn[i].addEventListener("click", function(){
+//         body.style.backgroundColor="green"
+
+//     })
+//     btn[i].addEventListener("mouseenter", function(){
+//         body.style.backgroundColor="blue"
+
+//     })
+// }
+    
+// btn.forEach((element) => {
+//     element.addEventListener("click", () => {
+//       document.body.style.backgroundColor = "green";
+//     });
+//   });
+
+// btn.onmouseenter=body.style.backgroundColor="blue"
