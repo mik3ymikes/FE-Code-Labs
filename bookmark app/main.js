ed18bffb701e35f457c28303185bb196efa196b7
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-09-20 22:17:20 -0500

    finish local storage adding and deleting

--- bookmark app/main.js
+++ bookmark app/main.js
@@ -21,23 +21,39 @@ function closeFloater(){
     }
 }
 
+localStorage.setItem("mything", 'something')
+
 const bookmarksList=document.querySelector(".bookmarks-list")
 const bookmarkForm=document.querySelector(".bookmark-form")
 const bookmarkInput=bookmarkForm.querySelector("input[type=text]")
+const bookmarks=JSON.parse(localStorage.getItem("bookmarks")) || []
+fillBookmarksList(bookmarks)
+
+
+console.table(bookmarks)
 
 function createBookmark(e){
     e.preventDefault()
 
-    console.log("processing form")
     const title=bookmarkInput.value 
-    const bookmark=document.createElement("a")
-    bookmark.className="bookmark"
-    bookmark.textContent=title
-    bookmark.href="#"
-    bookmark.target="_blank"
+    const bookmark={
+        title:title
+    }
+   bookmarks.push(bookmark)
+
+
+    // console.log("processing form")
+    // const title=bookmarkInput.value 
+    // const bookmark=document.createElement("a")
+    // bookmark.className="bookmark"
+    // bookmark.textContent=title
+    // bookmark.href="#"
+    // bookmark.target="_blank"
     
     
-    bookmarksList.appendChild(bookmark)
+    // bookmarksList.appendChild(bookmark)
+    fillBookmarksList(bookmarks)
+    storeBookmarks(bookmarks)
 
     bookmarkForm.reset()
 }
@@ -45,6 +61,36 @@ function createBookmark(e){
 
 
 
+function fillBookmarksList(bookmarks=[]){
+const bookmarksHtml=bookmarks.map((bookmark,i)=>{
+return `<a href="#" class="bookmark" data-id=${i}>
+<div class="img"></div>
+<div class="title">${bookmark.title} </div>
+<span class="glyphicon glyphicon-remove">😒</span></a> `
+}).join("")
+bookmarksList.innerHTML=bookmarksHtml
 
+// let bookmarksHtml=""
+// for(let i=0;i<bookmarks.length;i++){
+//     bookmarksHtml+=`<a href="#" class="bookmark"> ${bookmarks[i].title} </a> `
+// }
+// console.log(bookmarksHtml)
+}
 
 bookmarkForm.addEventListener("submit", createBookmark)
+bookmarksList.addEventListener("click",removeBookmark)
+
+
+function removeBookmark(e){
+if (!e.target.matches(".glyphicon-remove")) return;
+const index=e.target.parentNode.dataset.id
+bookmarks.splice(index, 1)
+fillBookmarksList(bookmarks)
+storeBookmarks(bookmarks)
+
+console.log("hey")
+}
+
+function storeBookmarks(bookmarks=[]){
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+}
